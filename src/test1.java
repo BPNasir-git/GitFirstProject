cd1cead9516ed2a0434244346b5c9bf085154a51
RHMaruf <mdrobiulhasan2620@gmail.com>
2020-08-25 12:19:22 +0600

    Second Commit

--- src/test1.java
+++ src/test1.java
@@ -1,5 +1,10 @@
 
 public class test1 {
+	
+	
+	public void testPrint() {
+		System.out.println("Another print Test");
+	}
 	public static void main(String args[]) {
 		System.out.println("Test");
 	}
