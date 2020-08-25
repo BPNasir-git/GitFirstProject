5d93cc777911b9d3d7ac29cfb8cab87bc67a1eb2
RHMaruf <mdrobiulhasan2620@gmail.com>
2020-08-25 15:11:39 +0600

    test 1 main method change

--- src/test1.java
+++ src/test1.java
@@ -3,7 +3,11 @@ public class test1 {
 	public void testPrint() {
 		System.out.println("Another print Test");
 	}
+	
 	public static void main(String args[]) {
 		System.out.println("Test");
+		test1 t = new test1();
+		t.testPrint();
 	}
+	
 }
